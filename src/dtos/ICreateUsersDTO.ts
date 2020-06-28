9837f6af4540ca03fdb71c8c46c430eb09165d1a
Fred Reis <fredgost@hotmail.com>
2020-06-28 13:53:52 -0300

    refactor: refactor dtos

--- src/dtos/ICreateUsersDTO.ts
+++ src/dtos/ICreateUsersDTO.ts
@@ -1,11 +1,19 @@
-export default interface ICreateUsersDTO {
-  nome: string;
-  cpf: string;
-  email: string;
-  senha: string;
+interface UserAddress {
   cep: string;
   rua: string;
   numero: number;
   bairro: string;
   cidade: string;
+}
+
+export default interface ICreateUsersDTO {
+  id?: string;
+  token?: string;
+  avatar?: string;
+  nome: string;
+  cpf: string;
+  sexo?: string;
+  email: string;
+  senha: string;
+  endereco: UserAddress;
 }
